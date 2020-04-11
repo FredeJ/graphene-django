1792ca4f5e3ed1a6580590c8a829201432caeb82
Frederik Juul <Frederikj@gmail.com>
2020-04-11 21:19:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     keywords="api graphql protocol rest relay graphene",
     packages=find_packages(exclude=["tests"]),
     install_requires=[
-        "graphene>=3,=3.0*",
+        "graphene>=3*",
         "graphql-core>=3",
         "Django>=3",
         "promise>=2.1",
